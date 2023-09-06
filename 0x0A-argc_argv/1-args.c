de019c79a7038d193937f92bbd3236d6ae0860a0
Macpricnce <princemacauley63@yahoo.com>
2023-09-06 06:34:52 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - the entry point of the program
+ * @argc: the arg count
+ * @argv: the arr of arg
+ * Return: always 0
+ */
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
